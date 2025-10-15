5f7bea3b514261f37c6620471c92b472a53fa7f1
Julian22077 <camilolozano0122@gmail.com>
2025-10-15 02:15:13 +0000

    sexto commit

--- js/Informativa.js
+++ js/Informativa.js
@@ -1,8 +1,8 @@
 function Informativa() {
     document.body.className = ""; 
     document.getElementById("root").innerHTML=`<section class="c-informativa">
-    <h1>R&M API</h1>
-    <h2>Julián Camilo Lozano Hernández</h>
+    <h1><a href="https://rickandmortyapi.com/api/character/">R&M API</a></h1>
+    <h2>Julián Camilo Lozano Hernández</h2>
     <img src="Logo.png">
     <p>Api con información de 826 personajes</p>
     </section>`;
